d285dd05ce3ad6e945de570b79d2bb5e7a2a115a
jiapril11 <tadadadacoding@gmail.com>
2023-10-13 02:11:00 +0900

    small changes

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
       <WidthLayout className="flex justify-between items-center">
         <h1>
           <Link href={"/"}>
-            <Image src={logo} alt="로고" />
+            <Image src={logo} alt="로고" priority />
           </Link>
         </h1>
         <nav className={kreon.className}>
