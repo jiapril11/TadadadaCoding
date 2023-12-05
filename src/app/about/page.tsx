cecb3411d53185f9d658bc61f764809187dcb21d
jiapril11 <tadadadacoding@gmail.com>
2023-12-05 14:54:33 +0900

    feat: added contact Form UI

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import WidthLayout from "../components/WidthLayout";
 import Profile from "../components/About/Profile";
+import ContactForm from "../components/About/ContactForm";
 
 export default function AboutPage() {
   return (
     <WidthLayout>
       <Profile />
+      <ContactForm />
     </WidthLayout>
   );
 }
