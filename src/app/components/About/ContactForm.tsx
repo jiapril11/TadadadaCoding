0a1acc25951a7eead06be223e7785d5bb21399e2
jiapril11 <tadadadacoding@gmail.com>
2023-12-06 15:14:44 +0900

    feat: added inform banner

--- src/app/components/About/ContactForm.tsx
+++ src/app/components/About/ContactForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import Banner, { BannerData } from "../Banner";
 
 export default function ContactForm() {
   const [emailData, setEmailData] = useState({
@@ -7,6 +8,8 @@ export default function ContactForm() {
     title: "",
     message: "",
   });
+
+  const [banner, setBanner] = useState<BannerData | null>(null);
 
   const handleChangeInput = (
     e:
@@ -18,61 +21,70 @@ export default function ContactForm() {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(emailData);
+    setBanner({ message: "메일이 전송되었습니다.", state: "success" });
+    setTimeout(() => {
+      setBanner(null);
+    }, 3000);
   };
 
+  const inputStyle = "w-full px-3 py-2 border border-gray-200 rounded-sm";
+  const labelTextStyle = "mb-2 text-sm text-gray-800 uppercase";
+
   return (
-    <section className="w-full max-w-lg min-w-[360px] mx-auto mt-10 mb-24">
-      <h2 className="mb-1 pr-1 text-xs font-semibold text-right uppercase text-gray-800">
-        Contact Me :&#41;
-      </h2>
-      <div className="border-2 border-gray-800 py-6 px-4 rounded-sm">
-        <form onSubmit={handleSubmit} className="space-y-5">
-          <div>
-            <label>
-              <p className="mb-2 text-sm text-gray-800 uppercase">Your Email</p>
-              <input
-                className="w-full px-3 py-2 border border-gray-200 rounded-sm"
-                type="text"
-                name="email"
-                placeholder="Your email address"
-                onChange={handleChangeInput}
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              <p className="mb-2 text-sm text-gray-800 uppercase">Title</p>
-              <input
-                className="w-full px-3 py-2 border border-gray-200 rounded-sm"
-                type="text"
-                name="title"
-                placeholder="Title of your message"
-                onChange={handleChangeInput}
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              <p className="mb-2 text-sm text-gray-800 uppercase">Message</p>
-              <textarea
-                className="w-full h-40 px-3 py-2 border border-gray-200 rounded-sm"
-                name="message"
-                placeholder="Type your message here."
-                onChange={handleChangeInput}
-              />
-            </label>
-          </div>
-          <div>
-            <button
-              type="submit"
-              className="block w-full py-2 bg-black text-white rounded-sm hover:bg-gray-800"
-            >
-              Submit
-            </button>
-          </div>
-        </form>
-      </div>
-    </section>
+    <>
+      <div className="h-10">{banner && <Banner banner={banner} />}</div>
+      <section className="w-full max-w-lg min-w-[360px] mx-auto mt-10 mb-24">
+        <h2 className="mb-1 pr-1 text-xs font-semibold text-right uppercase text-gray-800">
+          Contact Me :&#41;
+        </h2>
+        <div className="border-2 border-gray-800 py-6 px-4 rounded-sm">
+          <form onSubmit={handleSubmit} className="space-y-5">
+            <div>
+              <label>
+                <p className={labelTextStyle}>Your Email</p>
+                <input
+                  className={inputStyle}
+                  type="text"
+                  name="email"
+                  placeholder="Your email address"
+                  onChange={handleChangeInput}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                <p className={labelTextStyle}>Title</p>
+                <input
+                  className={inputStyle}
+                  type="text"
+                  name="title"
+                  placeholder="Title of your message"
+                  onChange={handleChangeInput}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                <p className={labelTextStyle}>Message</p>
+                <textarea
+                  className={inputStyle}
+                  name="message"
+                  placeholder="Type your message here."
+                  onChange={handleChangeInput}
+                />
+              </label>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="block w-full py-2 bg-black text-white rounded-sm hover:bg-gray-800"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
   );
 }
