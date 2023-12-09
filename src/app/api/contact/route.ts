4c385e38c7bbd9b648c124a67ab4c4392b7d4293
jiapril11 <tadadadacoding@gmail.com>
2023-12-09 21:29:11 +0900

    feat: add zod and react-hook-form, remove yup

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,24 +1,12 @@
 import { sendEamil } from "@/service/email";
-import * as yup from "yup";
-
-const boySchema = yup.object().shape({
-  from: yup.string().email().required(),
-  title: yup.string().required(),
-  message: yup.string().required(),
-});
 
 export async function POST(req: Request) {
   const body = await req.json();
-  if (!boySchema.isValidSync(body)) {
-    return new Response(JSON.stringify({ message: "전송 실패" }), {
-      status: 400,
-    });
-  }
 
   return sendEamil(body) //
     .then(
       () =>
-        new Response(JSON.stringify({ message: "성공적으로 전송됨" }), {
+        new Response(JSON.stringify({ message: "전송 성공" }), {
           status: 200,
         })
     )
--- src/app/components/About/ContactForm.tsx
+++ src/app/components/About/ContactForm.tsx
@@ -1,57 +1,75 @@
 "use client";
+
 import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Banner, { BannerData } from "../Banner";
 import { sendContactEmail } from "@/service/contact";
 
-type Form = {
-  from: string;
-  title: string;
-  message: string;
-};
-const DEFAULT_FORM = {
-  from: "",
-  title: "",
-  message: "",
-};
+const emailFormSchema = z.object({
+  from: z
+    .string()
+    .min(1, { message: "이메일을 입력해주세요" })
+    .email({ message: "유효하지 않은 이메일 형식입니다" }),
+  title: z.string().min(1, { message: "제목을 입력해주세요" }),
+  message: z.string().min(1, { message: "내용을 입력해주세요" }),
+});
+
+type EmailForm = z.infer<typeof emailFormSchema>;
 
 export default function ContactForm() {
-  const [emailData, setEmailData] = useState<Form>(DEFAULT_FORM);
-
   const [banner, setBanner] = useState<BannerData | null>(null);
 
-  const handleChangeInput = (
-    e:
-      | React.ChangeEvent<HTMLInputElement>
-      | React.ChangeEvent<HTMLTextAreaElement>
-  ) => {
-    setEmailData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    reset,
+  } = useForm<EmailForm>({
+    resolver: zodResolver(emailFormSchema),
+    mode: "onChange",
+    defaultValues: {
+      from: "",
+      title: "",
+      message: "",
+    },
+  });
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    sendContactEmail(emailData)
-      .then(() => {
-        setBanner({
-          message: "메일이 성공적으로 전송되었습니다.",
-          state: "success",
+  const onHandleSubmit: SubmitHandler<EmailForm> = (emailData) => {
+    if (emailFormSchema.safeParse(emailData)) {
+      sendContactEmail(emailData)
+        .then(() => {
+          reset();
+          setBanner({
+            message: "메일이 성공적으로 전송되었습니다.",
+            state: "success",
+          });
+        })
+        .catch((error) => {
+          setBanner({
+            message: "메일 전송이 실패되었습니다. 다시 시도해주세요.",
+            state: "error",
+          });
+        })
+        .finally(() => {
+          setTimeout(() => {
+            setBanner(null);
+          }, 3000);
         });
-        setEmailData(DEFAULT_FORM);
-      })
-      .catch((error) => {
-        setBanner({
-          message: "메일 전송이 실패되었습니다. 다시 시도해주세요.",
-          state: "error",
-        });
-      })
-      .finally(() => {
-        setTimeout(() => {
-          setBanner(null);
-        }, 3000);
-      });
+    }
   };
 
   const inputStyle = "w-full px-3 py-2 border border-gray-200 rounded-sm";
-  const labelTextStyle = "mb-2 text-sm text-gray-800 uppercase";
+  const labelWrapperStyle = "flex justify-between align-center mb-2 px-1";
+  const labelTextStyle = "text-sm text-gray-800 uppercase";
+  const validateTextStyle = "text-red-500 text-xs";
+  const validateInputStyle = (name: string) =>
+    `${inputStyle} ${
+      errors[name as keyof EmailForm]?.message
+        ? "border-red-500 focus:outline-red-500"
+        : "border-gray-200"
+    }`;
 
   return (
     <>
@@ -61,49 +79,68 @@ export default function ContactForm() {
           Contact Me :&#41;
         </h2>
         <div className="border-2 border-gray-800 py-6 px-4 rounded-sm">
-          <form onSubmit={handleSubmit} className="space-y-5">
+          <form onSubmit={handleSubmit(onHandleSubmit)} className="space-y-5">
             <div>
-              <label>
-                <p className={labelTextStyle}>Your Email</p>
-                <input
-                  className={inputStyle}
-                  type="text"
-                  name="from"
-                  value={emailData.from}
-                  placeholder="Your email address"
-                  onChange={handleChangeInput}
-                />
-              </label>
+              <div className={labelWrapperStyle}>
+                <label className={labelTextStyle} htmlFor="from">
+                  Your Email
+                </label>
+                {errors.from && (
+                  <span className={validateTextStyle}>
+                    {errors.from.message}
+                  </span>
+                )}
+              </div>
+              <input
+                id="from"
+                className={validateInputStyle("from")}
+                type="text"
+                placeholder="Your email address"
+                {...register("from")}
+              />
             </div>
             <div>
-              <label>
-                <p className={labelTextStyle}>Title</p>
-                <input
-                  className={inputStyle}
-                  type="text"
-                  name="title"
-                  value={emailData.title}
-                  placeholder="Title of your message"
-                  onChange={handleChangeInput}
-                />
-              </label>
+              <div className={labelWrapperStyle}>
+                <label className={labelTextStyle} htmlFor="title">
+                  Title
+                </label>
+                {errors.title && (
+                  <span className={validateTextStyle}>
+                    {errors.title.message}
+                  </span>
+                )}
+              </div>
+              <input
+                id="title"
+                className={validateInputStyle("title")}
+                type="text"
+                placeholder="Title of your message"
+                {...register("title")}
+              />
             </div>
             <div>
-              <label>
-                <p className={labelTextStyle}>Message</p>
-                <textarea
-                  className={`${inputStyle} h-52`}
-                  name="message"
-                  value={emailData.message}
-                  placeholder="Type your message here."
-                  onChange={handleChangeInput}
-                />
-              </label>
+              <div className={labelWrapperStyle}>
+                <label className={labelTextStyle} htmlFor="message">
+                  Message
+                </label>
+                {errors.message && (
+                  <span className={validateTextStyle}>
+                    {errors.message.message}
+                  </span>
+                )}
+              </div>
+              <textarea
+                id="message"
+                className={`${validateInputStyle("message")} h-52`}
+                placeholder="Type your message here."
+                {...register("message")}
+              />
             </div>
             <div>
               <button
                 type="submit"
-                className="block w-full py-2 bg-black text-white rounded-sm hover:bg-gray-800"
+                disabled={!isValid}
+                className="block w-full py-2 bg-gray-400 enabled:bg-black text-white rounded-sm enabled:hover:bg-gray-800"
               >
                 Submit
               </button>
